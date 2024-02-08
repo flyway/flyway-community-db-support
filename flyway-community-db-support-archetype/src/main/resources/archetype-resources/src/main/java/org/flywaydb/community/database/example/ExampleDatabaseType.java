f3d5553f83ee7ad3096552c310c1d86702a2413e
Barry Attwater <barry.attwater@red-gate.com>
2024-02-08 16:35:31 +0000

    Add a flyway-community-db-support-archetype

---
+++ flyway-community-db-support-archetype/src/main/resources/archetype-resources/src/main/java/org/flywaydb/community/database/example/ExampleDatabaseType.java
@@ -0,0 +1,53 @@
+package org.flywaydb.community.database.example;
+
+import java.sql.Connection;
+import org.flywaydb.core.api.ResourceProvider;
+import org.flywaydb.core.api.configuration.Configuration;
+import org.flywaydb.core.internal.database.base.BaseDatabaseType;
+import org.flywaydb.core.internal.database.base.Database;
+import org.flywaydb.core.internal.jdbc.JdbcConnectionFactory;
+import org.flywaydb.core.internal.jdbc.StatementInterceptor;
+import org.flywaydb.core.internal.parser.Parser;
+import org.flywaydb.core.internal.parser.ParsingContext;
+
+public class ExampleDatabaseType extends BaseDatabaseType {
+
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  @Override
+  public int getNullType() {
+    return 0;
+  }
+
+  @Override
+  public boolean handlesJDBCUrl(final String url) {
+    return false;
+  }
+
+  @Override
+  public String getDriverClass(final String url, final ClassLoader classLoader) {
+    return null;
+  }
+
+  @Override
+  public boolean handlesDatabaseProductNameAndVersion(final String databaseProductName,
+      final String databaseProductVersion,
+      final Connection connection) {
+    return false;
+  }
+
+  @Override
+  public Database createDatabase(final Configuration configuration, final JdbcConnectionFactory jdbcConnectionFactory,
+      final StatementInterceptor statementInterceptor) {
+    return null;
+  }
+
+  @Override
+  public Parser createParser(final Configuration configuration, final ResourceProvider resourceProvider,
+      final ParsingContext parsingContext) {
+    return null;
+  }
+}
