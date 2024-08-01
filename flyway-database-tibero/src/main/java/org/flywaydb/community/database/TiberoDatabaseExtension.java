37985aa8277d9ac21ba6a0f445087f11e8aea78a
YGwan <good0619@naver.com>
2024-08-01 10:00:04 +0900

    flyway for tibero database extension

--- flyway-database-tibero/src/main/java/org/flywaydb/community/database/TiberoDatabaseExtension.java
+++ flyway-database-tibero/src/main/java/org/flywaydb/community/database/TiberoDatabaseExtension.java
@@ -1,6 +1,27 @@
 package org.flywaydb.community.database;
 
+import org.flywaydb.core.api.FlywayException;
 import org.flywaydb.core.extensibility.PluginMetadata;
+import org.flywaydb.core.internal.util.FileUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class TiberoDatabaseExtension implements PluginMetadata {
+
+    public String getDescription() {
+        return "Community-contributed Tibero database support extension " + readVersion() + " by Redgate";
+    }
+
+    public static String readVersion() {
+        try {
+            return FileUtils.copyToString(
+                    Objects.requireNonNull(TiberoDatabaseExtension.class.getClassLoader()
+                            .getResourceAsStream("org/flywaydb/community/database/tibero/version.txt")),
+                    StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new FlywayException("Unable to read extension version: " + e.getMessage(), e);
+        }
+    }
 }
