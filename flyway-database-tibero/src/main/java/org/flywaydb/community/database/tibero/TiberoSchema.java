d3736651de1c51eef6fb7d797161d3261566ca44
YGwan <good0619@naver.com>
2024-08-01 11:26:04 +0900

    add tibero schema schedule, sql_translation_profile, materialized_view, materialized_view_log, dimension

--- flyway-database-tibero/src/main/java/org/flywaydb/community/database/tibero/TiberoSchema.java
+++ flyway-database-tibero/src/main/java/org/flywaydb/community/database/tibero/TiberoSchema.java
@@ -1,23 +1,19 @@
 package org.flywaydb.community.database.tibero;
 
 
-import static org.flywaydb.community.database.tibero.TiberoSchema.ObjectType.QUEUE_TABLE;
-import static org.flywaydb.community.database.tibero.TiberoSchema.ObjectType.SCHEDULER_CHAIN;
-import static org.flywaydb.community.database.tibero.TiberoSchema.ObjectType.SCHEDULER_JOB;
-import static org.flywaydb.community.database.tibero.TiberoSchema.ObjectType.SCHEDULER_PROGRAM;
-import static org.flywaydb.community.database.tibero.TiberoSchema.ObjectType.TRIGGER;
-
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.flywaydb.core.api.FlywayException;
 import org.flywaydb.core.internal.database.base.Schema;
 import org.flywaydb.core.internal.database.base.Table;
 import org.flywaydb.core.internal.jdbc.JdbcTemplate;
 import org.flywaydb.core.internal.util.StringUtils;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.flywaydb.community.database.tibero.TiberoSchema.ObjectType.*;
 
 public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
 
@@ -52,7 +48,7 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
     protected void doClean() throws SQLException {
         if (isSystem()) {
             throw new FlywayException("Clean not supported on Tibero for system schema " + database.quote(name) + "! " +
-                "It must not be changed in any way except by running an Tibero-supplied script!");
+                    "It must not be changed in any way except by running an Tibero-supplied script!");
         }
 
         if (database.isFlashbackDataArchiveAvailable(name)) {
@@ -66,12 +62,17 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         Set<String> objectTypeNames = ObjectType.getObjectTypeNames(jdbcTemplate, database, this);
 
         List<ObjectType> objectTypesToClean = List.of(
-            TRIGGER,
-            QUEUE_TABLE,
-            SCHEDULER_CHAIN,
-            SCHEDULER_JOB,
-            SCHEDULER_PROGRAM
-            );
+                TRIGGER,
+                QUEUE_TABLE,
+                SCHEDULER_CHAIN,
+                SCHEDULER_JOB,
+                SCHEDULER_PROGRAM,
+                SCHEDULE,
+                SQL_TRANSLATION_PROFILE,
+                MATERIALIZED_VIEW,
+                MATERIALIZED_VIEW_LOG,
+                DIMENSION
+        );
 
         for (ObjectType objectType : objectTypesToClean) {
             if (objectTypeNames.contains(objectType.getName())) {
@@ -102,6 +103,10 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         jdbcTemplate.getConnection().commit();
     }
 
+    private boolean locatorMetadataExists() throws SQLException {
+        return database.queryReturnsRows("SELECT * FROM ALL_GEOMETRY_COLUMNS WHERE F_TABLE_SCHEMA = ?",
+                name);
+    }
 
     @Override
     protected TiberoTable[] doAllTables() throws SQLException {
@@ -114,38 +119,63 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         return new TiberoTable(jdbcTemplate, database, this, tableName);
     }
 
+    private void disableFlashbackArchiveForFbaTrackedTables() throws SQLException {
+        boolean dbaViewAccessible = database.isPrivOrRoleGranted("SELECT ANY DICTIONARY")
+                || database.isDataDictViewAccessible("DBMS_FLASHBACK");
+
+        if (!dbaViewAccessible && !isDefaultSchemaForUser()) {
+            return;
+        }
+
+        String queryForRecycleBinStatus =
+                "SELECT VALUE FROM " + database.quote("SYS", "V$PARAMETERS") + " WHERE NAME = 'USE_RECYCLEBIN'";
+        String recycleBinStatus = jdbcTemplate.queryForString(queryForRecycleBinStatus);
+
+        if (!"NO".equalsIgnoreCase(recycleBinStatus)) {
+            jdbcTemplate.execute("ALTER SYSTEM SET USE_RECYCLEBIN = N");
+
+            while (!"NO".equalsIgnoreCase(jdbcTemplate.queryForString(queryForRecycleBinStatus))) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new FlywayException("Interrupted while waiting for recycle bin to be disabled.", e);
+                }
+            }
+        }
+    }
+
     public enum ObjectType {
         TRIGGER("TRIGGER"),
         QUEUE_TABLE("QUEUE TABLE") {
             @Override
             public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema) throws SQLException {
+                                               TiberoSchema schema) throws SQLException {
                 return jdbcTemplate.queryForStringList(
-                    "SELECT QUEUE_TABLE FROM ALL_QUEUE_TABLES WHERE OWNER = ?",
-                    schema.getName()
+                        "SELECT QUEUE_TABLE FROM ALL_QUEUE_TABLES WHERE OWNER = ?",
+                        schema.getName()
                 );
             }
 
             @Override
             public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema, String objectName) {
+                                                TiberoSchema schema, String objectName) {
                 return "BEGIN DBMS_AQADM.DROP_QUEUE_TABLE('" + objectName + "'); END;";
             }
         },
         SCHEDULER_CHAIN("SCHEDULER_CHAINS") {
             @Override
             public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema) throws SQLException {
+                                               TiberoSchema schema) throws SQLException {
                 return jdbcTemplate.queryForStringList(
-                    "SELECT CHAIN_NAME FROM " + database.dbaOrAll(SCHEDULER_CHAIN.getName()) + " WHERE OWNER_ID IN ("
-                        + "SELECT DISTINCT USER_ID FROM ALL_USERS WHERE USERNAME = ? "
-                        + ") ", schema.getName()
+                        "SELECT CHAIN_NAME FROM " + database.dbaOrAll(SCHEDULER_CHAIN.getName()) + " WHERE OWNER_ID IN ("
+                                + "SELECT DISTINCT USER_ID FROM ALL_USERS WHERE USERNAME = ? "
+                                + ") ", schema.getName()
                 );
             }
 
             @Override
             public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema, String objectName) {
+                                                TiberoSchema schema, String objectName) {
 
                 deleteChainRules(jdbcTemplate, database, objectName);
                 deleteChainSteps(jdbcTemplate, database, objectName);
@@ -157,12 +187,12 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
                     final String SCHEDULER_JOB_RULE_TABLE_NAME = database.dbaOrAll("SCHEDULER_RULES");
 
                     List<String> ruleNames = jdbcTemplate.queryForStringList(
-                        "SELECT RULE_NAME FROM " + SCHEDULER_JOB_RULE_TABLE_NAME + " WHERE CHAIN_NAME = ?",
-                        objectName);
+                            "SELECT RULE_NAME FROM " + SCHEDULER_JOB_RULE_TABLE_NAME + " WHERE CHAIN_NAME = ?",
+                            objectName);
 
                     for (String ruleName : ruleNames) {
                         jdbcTemplate.execute(
-                            "BEGIN DBMS_SCHEDULER.DROP_CHAIN_RULE('" + objectName + "','" + ruleName + "'); END;");
+                                "BEGIN DBMS_SCHEDULER.DROP_CHAIN_RULE('" + objectName + "','" + ruleName + "'); END;");
                     }
 
                 } catch (SQLException e) {
@@ -174,12 +204,12 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
                     final String SCHEDULER_JOB_STEP_TABLE_NAME = database.dbaOrAll("SCHEDULER_STEPS");
 
                     List<String> stepNames = jdbcTemplate.queryForStringList(
-                        "SELECT STEP_NAME FROM " + SCHEDULER_JOB_STEP_TABLE_NAME + " WHERE CHAIN_NAME = ?",
-                        objectName);
+                            "SELECT STEP_NAME FROM " + SCHEDULER_JOB_STEP_TABLE_NAME + " WHERE CHAIN_NAME = ?",
+                            objectName);
 
                     for (String stepName : stepNames) {
                         jdbcTemplate.execute(
-                            "BEGIN DBMS_SCHEDULER.DROP_CHAIN_STEP('" + objectName + "','" + stepName + "'); END;");
+                                "BEGIN DBMS_SCHEDULER.DROP_CHAIN_STEP('" + objectName + "','" + stepName + "'); END;");
                     }
 
                 } catch (SQLException e) {
@@ -190,22 +220,22 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         SCHEDULER_JOB("SCHEDULER_JOBS") {
             @Override
             public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema) throws SQLException {
+                                               TiberoSchema schema) throws SQLException {
                 return jdbcTemplate.queryForStringList(
-                    "SELECT JOB_NAME FROM " + database.dbaOrAll(SCHEDULER_JOB.getName()) + " WHERE OWNER = ?",
-                    schema.getName()
+                        "SELECT JOB_NAME FROM " + database.dbaOrAll(SCHEDULER_JOB.getName()) + " WHERE OWNER = ?",
+                        schema.getName()
                 );
             }
 
             @Override
             public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema, String objectName) {
+                                                TiberoSchema schema, String objectName) {
 
                 try {
                     final String SCHEDULER_JOB_LOG_TABLE_NAME = database.dbaOrAll("SCHEDULER_JOB_LOG");
 
                     jdbcTemplate.update("DELETE FROM " + SCHEDULER_JOB_LOG_TABLE_NAME + " WHERE JOB_NAME = ?",
-                        objectName);
+                            objectName);
                 } catch (SQLException e) {
                 }
 
@@ -216,17 +246,74 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         SCHEDULER_PROGRAM("SCHEDULER_PROGRAMS") {
             @Override
             public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema) throws SQLException {
+                                               TiberoSchema schema) throws SQLException {
                 return jdbcTemplate.queryForStringList(
-                    "SELECT PROGRAM_NAME FROM " + database.dbaOrAll(SCHEDULER_PROGRAM.getName()) + " WHERE OWNER = ?",
-                    schema.getName()
+                        "SELECT PROGRAM_NAME FROM " + database.dbaOrAll(SCHEDULER_PROGRAM.getName()) + " WHERE OWNER = ?",
+                        schema.getName()
                 );
             }
 
             @Override
             public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-                TiberoSchema schema, String objectName) {
+                                                TiberoSchema schema, String objectName) {
                 return "BEGIN DBMS_SCHEDULER.DROP_PROGRAM('" + objectName + "'); END;";
+            }
+        },
+
+        SCHEDULE("SCHEDULER_SCHEDULES") {
+            @Override
+            public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+                                               TiberoSchema schema) throws SQLException {
+                return jdbcTemplate.queryForStringList(
+                        "SELECT SCHEDULE_NAME FROM " + database.dbaOrAll(SCHEDULE.getName()) + " WHERE OWNER = ?",
+                        schema.getName()
+                );
+            }
+
+            @Override
+            public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+                                                TiberoSchema schema, String objectName) {
+                return "BEGIN DBMS_SCHEDULER.DROP_SCHEDULE('" + objectName + "'); END;";
+            }
+        },
+
+        SQL_TRANSLATION_PROFILE("SQL TRANSLATION PROFILE") {
+            @Override
+            public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+                                                TiberoSchema schema, String objectName) {
+                return "BEGIN DBMS_SQL_TRANSLATOR.DROP_PROFILE('" + database.quote(schema.getName(),
+                        objectName) + "'); END;";
+            }
+        },
+
+        MATERIALIZED_VIEW("MATERIALIZED VIEW", "PRESERVE TABLE"),
+
+        MATERIALIZED_VIEW_LOG("MATERIALIZED VIEW LOG") {
+            @Override
+            public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+                                               TiberoSchema schema) throws SQLException {
+                return jdbcTemplate.queryForStringList(
+                        "SELECT MASTER FROM ALL_MVIEW_LOGS WHERE LOG_OWNER = ?",
+                        schema.getName()
+                );
+            }
+
+            @Override
+            public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+                                                TiberoSchema schema, String objectName) {
+                return "DROP " + this.getName() + " ON " + database.quote(schema.getName(),
+                        objectName);
+            }
+        },
+
+        DIMENSION("DIMENSION") {
+            @Override
+            public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+                                               TiberoSchema schema) throws SQLException {
+                return jdbcTemplate.queryForStringList(
+                        "SELECT 'DIMENSION' FROM DUAL",
+                        schema.getName()
+                );
             }
         },
         ;
@@ -253,24 +340,24 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         }
 
         public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-            TiberoSchema schema) throws SQLException {
+                                           TiberoSchema schema) throws SQLException {
             return jdbcTemplate.queryForStringList(
-                "SELECT DISTINCT OBJECT_NAME FROM ALL_OBJECTS WHERE OWNER = ? AND OBJECT_TYPE = ?",
-                schema.getName(), this.getName()
+                    "SELECT DISTINCT OBJECT_NAME FROM ALL_OBJECTS WHERE OWNER = ? AND OBJECT_TYPE = ?",
+                    schema.getName(), this.getName()
             );
         }
 
         public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-            TiberoSchema schema, String objectName) {
+                                            TiberoSchema schema, String objectName) {
             return "DROP " + this.getName() + " " + database.quote(schema.getName(), objectName) +
-                (StringUtils.hasText(dropOptions) ? " " + dropOptions : "");
+                    (StringUtils.hasText(dropOptions) ? " " + dropOptions : "");
         }
 
         public void dropObjects(JdbcTemplate jdbcTemplate, TiberoDatabase database,
-            TiberoSchema schema) throws SQLException {
+                                TiberoSchema schema) throws SQLException {
             for (String objectName : getObjectNames(jdbcTemplate, database, schema)) {
                 jdbcTemplate.execute(
-                    generateDropStatement(jdbcTemplate, database, schema, objectName));
+                        generateDropStatement(jdbcTemplate, database, schema, objectName));
             }
         }
 
@@ -280,6 +367,44 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
 
         private void warnUnsupported(String schemaName) {
             warnUnsupported(schemaName, this.toString().toLowerCase() + "s");
+        }
+
+        public static Set<String> getObjectTypeNames(JdbcTemplate jdbcTemplate,
+                                                     TiberoDatabase database, TiberoSchema schema) throws SQLException {
+            String query =
+                    "SELECT DISTINCT OBJECT_TYPE FROM " + database.dbaOrAll("OBJECTS") + " WHERE OWNER = ? "
+                            +
+                            "UNION SELECT '" + MATERIALIZED_VIEW_LOG.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM ALL_MVIEW_LOGS WHERE LOG_OWNER = ?) "
+                            +
+                            "UNION SELECT '" + QUEUE_TABLE.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM ALL_QUEUE_TABLES WHERE OWNER = ?) "
+                            +
+                            "UNION SELECT '" + DATABASE_LINK.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM " + database.dbaOrAll("DB_LINKS") + " WHERE OWNER = ?) " +
+                            "UNION SELECT '" + CONTEXT.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM V$CONTEXT WHERE NAMESPACE = ?) "
+                            +
+                            "UNION SELECT '" + CREDENTIAL.getName() + "' FROM DUAL "
+                            +
+                            "UNION SELECT '" + SCHEDULER_PROGRAM.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM " + database.dbaOrAll(SCHEDULER_PROGRAM.getName()) + " WHERE OWNER = ?) "
+                            +
+                            "UNION SELECT '" + SCHEDULE.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM " + database.dbaOrAll(SCHEDULE.getName()) + " WHERE OWNER = ?) "
+                            +
+                            "UNION SELECT '" + SCHEDULER_JOB.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM " + database.dbaOrAll(SCHEDULER_JOB.getName()) + " WHERE OWNER = ?) "
+                            +
+                            "UNION SELECT '" + SCHEDULER_CHAIN.getName() + "' FROM DUAL WHERE EXISTS(" +
+                            "SELECT * FROM " + database.dbaOrAll(SCHEDULER_CHAIN.getName()) + " WHERE OWNER_ID IN ("
+                            + "SELECT DISTINCT USER_ID FROM ALL_USERS WHERE USERNAME = ?)"
+                            + ") ";
+
+            String[] params = new String[9];
+            Arrays.fill(params, schema.getName());
+
+            return new HashSet<>(jdbcTemplate.queryForStringList(query, params));
         }
     }
 }
