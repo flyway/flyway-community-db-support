11f9b1d321d4b153991b8c9d38cad911c9befabe
birdie <flaehdan@gmail.com>
2024-08-01 11:05:51 +0900

    add tibero schema

--- flyway-database-tibero/src/main/java/org/flywaydb/community/database/tibero/TiberoSchema.java
+++ flyway-database-tibero/src/main/java/org/flywaydb/community/database/tibero/TiberoSchema.java
@@ -2,9 +2,15 @@ package org.flywaydb.community.database.tibero;
 
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.flywaydb.core.internal.database.base.Schema;
 import org.flywaydb.core.internal.database.base.Table;
 import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.flywaydb.core.internal.util.StringUtils;
 
 public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
 
@@ -12,24 +18,27 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
         super(jdbcTemplate, database, name);
     }
 
+    boolean isDefaultSchemaForUser() throws SQLException {
+        return name.equals(database.doGetCurrentUser());
+    }
+
     @Override
     protected boolean doExists() throws SQLException {
-        return false;
+        return database.queryReturnsRows("SELECT * FROM ALL_USERS WHERE USERNAME = ?", name);
     }
 
     @Override
     protected boolean doEmpty() throws SQLException {
-        return false;
+        return true;
     }
 
     @Override
     protected void doCreate() throws SQLException {
-
     }
 
     @Override
     protected void doDrop() throws SQLException {
-
+        jdbcTemplate.execute("DROP USER " + database.quote(name) + " CASCADE");
     }
 
     @Override
@@ -39,11 +48,68 @@ public class TiberoSchema extends Schema<TiberoDatabase, TiberoTable> {
 
     @Override
     protected TiberoTable[] doAllTables() throws SQLException {
-        return new TiberoTable[0];
+        return new TiberoTable[1];
     }
 
     @Override
-    public Table getTable(String s) {
-        return null;
+    @SuppressWarnings("rawtypes")
+    public Table getTable(String tableName) {
+        return new TiberoTable(jdbcTemplate, database, this, tableName);
+    }
+
+    public enum ObjectType {
+        TABLE("TABLE"),
+        ;
+
+        private final String name;
+        private final String dropOptions;
+
+        ObjectType(String name, String dropOptions) {
+            this.name = name;
+            this.dropOptions = dropOptions;
+        }
+
+        ObjectType(String name) {
+            this(name, "");
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString().replace('_', ' ');
+        }
+
+        public List<String> getObjectNames(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+            TiberoSchema schema) throws SQLException {
+            return jdbcTemplate.queryForStringList(
+                "SELECT DISTINCT OBJECT_NAME FROM ALL_OBJECTS WHERE OWNER = ? AND OBJECT_TYPE = ?",
+                schema.getName(), this.getName()
+            );
+        }
+
+        public String generateDropStatement(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+            TiberoSchema schema, String objectName) {
+            return "DROP " + this.getName() + " " + database.quote(schema.getName(), objectName) +
+                (StringUtils.hasText(dropOptions) ? " " + dropOptions : "");
+        }
+
+        public void dropObjects(JdbcTemplate jdbcTemplate, TiberoDatabase database,
+            TiberoSchema schema) throws SQLException {
+            for (String objectName : getObjectNames(jdbcTemplate, database, schema)) {
+                jdbcTemplate.execute(
+                    generateDropStatement(jdbcTemplate, database, schema, objectName));
+            }
+        }
+
+        private void warnUnsupported(String schemaName, String typeDesc) {
+
+        }
+
+        private void warnUnsupported(String schemaName) {
+            warnUnsupported(schemaName, this.toString().toLowerCase() + "s");
+        }
     }
 }
