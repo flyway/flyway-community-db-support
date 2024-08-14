c120dc507bc5390528549a4098901a5a507bdb8a
YGwan <good0619@naver.com>
2024-08-14 18:17:39 +0900

    add test_sql for validate test

---
+++ flyway-database-tibero/src/test/resources/db/migration-with-failed/V1__create_test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE TEST
+(
+    test_id    VARCHAR2(255) PRIMARY KEY,
+    title        VARCHAR2(255),
+    content  VARCHAR2(4000)
+);
---
+++ flyway-database-tibero/src/test/resources/db/migration/V1__create_test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE TEST
+(
+    test_id    NUMBER(19) PRIMARY KEY,
+    title        VARCHAR2(255),
+    content  VARCHAR2(4000)
+);
