ba30e801f261aacacd1c944299d45be85ec3c44c
zyh <xxsc0529@163.com>
2024-09-24 17:10:27 +0800

    Feature:add oceanbase oracle mode support

--- flyway-database-oceanbase/src/main/java/org/flywaydb/community/database/oceanbase/mysql/OceanBaseMysqlModeDatabase.java
+++ flyway-database-oceanbase/src/main/java/org/flywaydb/community/database/oceanbase/mysql/OceanBaseMysqlModeDatabase.java
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * flyway-database-oceanbase
+ * ========================================================================
+ * Copyright (C) 2010 - 2024 Red Gate Software Ltd
+ * ========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 /*
  * Copyright (C) Red Gate Software Ltd 2010-2024
  *
@@ -13,7 +32,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flywaydb.community.database.oceanbase;
+package org.flywaydb.community.database.oceanbase.mysql;
+
+import org.flywaydb.community.database.oceanbase.OceanBaseJdbcUtils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.flywaydb.core.api.FlywayException;
 import org.flywaydb.core.api.MigrationVersion;
@@ -23,18 +47,15 @@ import org.flywaydb.core.internal.jdbc.StatementInterceptor;
 import org.flywaydb.database.mysql.MySQLConnection;
 import org.flywaydb.database.mysql.MySQLDatabase;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+public class OceanBaseMysqlModeDatabase extends MySQLDatabase {
 
-public class OceanBaseDatabase extends MySQLDatabase {
-
-    public OceanBaseDatabase(Configuration configuration, JdbcConnectionFactory jdbcConnectionFactory, StatementInterceptor statementInterceptor) {
+    public OceanBaseMysqlModeDatabase(Configuration configuration, JdbcConnectionFactory jdbcConnectionFactory, StatementInterceptor statementInterceptor) {
         super(configuration, jdbcConnectionFactory, statementInterceptor);
     }
 
     @Override
     protected MySQLConnection doGetConnection(Connection connection) {
-        return new OceanBaseConnection(this, connection);
+        return new OceanBaseMysqlModeConnection(this, connection);
     }
 
     @Override
