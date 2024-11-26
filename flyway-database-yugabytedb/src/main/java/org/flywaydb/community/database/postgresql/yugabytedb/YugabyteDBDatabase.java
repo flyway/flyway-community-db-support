6b4e98ad6b056025144ebd4dfde215b6dffebbde
Amogh Shetkar <ashetkar@yugabyte.com>
2024-11-26 07:06:39 +0000

    Ensure that only the thread which sets the lock_id, resets it

--- flyway-database-yugabytedb/src/main/java/org/flywaydb/community/database/postgresql/yugabytedb/YugabyteDBDatabase.java
+++ flyway-database-yugabytedb/src/main/java/org/flywaydb/community/database/postgresql/yugabytedb/YugabyteDBDatabase.java
@@ -31,12 +31,13 @@ import java.sql.SQLException;
 public class YugabyteDBDatabase extends PostgreSQLDatabase {
 
     public static final String LOCK_TABLE_NAME = "YB_FLYWAY_LOCK_TABLE";
+    private static final String DROP_TABLE_IF_EXISTS = "DROP TABLE IF EXISTS " + LOCK_TABLE_NAME;
     /**
      * This table is used to enforce locking through SELECT ... FOR UPDATE on a
      * token row inserted in this table. The token row is inserted with the name
      * of the Flyway's migration history table as a token for simplicity.
      */
-    private static final String CREATE_LOCK_TABLE_DDL = "CREATE TABLE IF NOT EXISTS " + LOCK_TABLE_NAME + " (table_name varchar PRIMARY KEY, locked bool)";
+    private static final String CREATE_LOCK_TABLE_DDL = "CREATE TABLE IF NOT EXISTS " + LOCK_TABLE_NAME + " (table_name varchar PRIMARY KEY, lock_id bigint)";
 
     public YugabyteDBDatabase(Configuration configuration, JdbcConnectionFactory jdbcConnectionFactory, StatementInterceptor statementInterceptor) {
         super(configuration, jdbcConnectionFactory, statementInterceptor);
@@ -84,6 +85,7 @@ public class YugabyteDBDatabase extends PostgreSQLDatabase {
 
     private void createLockTable() {
         try {
+            jdbcTemplate.execute(DROP_TABLE_IF_EXISTS);
             jdbcTemplate.execute(CREATE_LOCK_TABLE_DDL);
         } catch (SQLException e) {
             throw new FlywaySqlException("Unable to initialize the lock table", e);
