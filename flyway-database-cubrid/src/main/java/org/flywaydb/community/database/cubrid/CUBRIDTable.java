5e94cc1aae81812f61de26aa9c65344feb8e5f34
GwangCheonLee <raitmid56@gmail.com>
2025-03-04 21:13:27 +0900

    Supported Cubrid

---
+++ flyway-database-cubrid/src/main/java/org/flywaydb/community/database/cubrid/CUBRIDTable.java
@@ -0,0 +1,73 @@
+/*-
+ * ========================LICENSE_START=================================
+ * flyway-database-cubrid
+ * ========================================================================
+ * Copyright (C) 2010 - 2025 Red Gate Software Ltd
+ * ========================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+/*
+ * Copyright (C) Red Gate Software Ltd 2010-2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.community.database.cubrid;
+
+import java.sql.SQLException;
+import org.flywaydb.core.internal.database.base.Table;
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+
+public class CUBRIDTable extends Table<CUBRIDDatabase, CUBRIDSchema> {
+
+    /**
+     * @param jdbcTemplate The JDBC template for communicating with the DB.
+     * @param database     The database-specific support.
+     * @param schema       The schema this table lives in.
+     * @param name         The name of the table.
+     */
+    public CUBRIDTable(JdbcTemplate jdbcTemplate, CUBRIDDatabase database, CUBRIDSchema schema,
+        String name) {
+        super(jdbcTemplate, database, schema, name);
+    }
+
+    @Override
+    protected boolean doExists() throws SQLException {
+        return jdbcTemplate.queryForInt(
+            "SELECT COUNT(*) FROM db_class WHERE class_name = ? AND is_system_class = 'NO'",
+            name
+        ) > 0;
+    }
+
+
+    @Override
+    protected void doLock() {
+        // CUBRID does not support table level locks
+    }
+
+    @Override
+    protected void doDrop() throws SQLException {
+        jdbcTemplate.execute("DROP TABLE IF EXISTS " + database.quote(name));
+    }
+
+}
