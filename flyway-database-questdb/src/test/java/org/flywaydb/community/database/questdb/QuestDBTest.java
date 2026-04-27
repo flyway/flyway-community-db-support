1f837d44df3c06c54a1fba2df5b1965e7a2db0da
ibi107 <ibtesam.ahmad@red-gate.com>
2026-04-27 14:32:23 +0100

    Bump assert query timeout

--- flyway-database-questdb/src/test/java/org/flywaydb/community/database/questdb/QuestDBTest.java
+++ flyway-database-questdb/src/test/java/org/flywaydb/community/database/questdb/QuestDBTest.java
@@ -32,7 +32,7 @@ import static org.junit.Assert.*;
 
 public class QuestDBTest {
     private static final DockerImageName QUESTDB_IMAGE = DockerImageName.parse("questdb/questdb:9.3.5");
-    private static final long ASSERT_QUERY_TIMEOUT = 10000L;
+    private static final long ASSERT_QUERY_TIMEOUT = 30000L;
     private static final int HTTP_PORT = 9000;
     private static final int PG_PORT = 8812;
     private static final String LOCATION = "questdb_migration";
